6a3866ed409b25620ebb4515a7124ac1166affab
Justa <9180155@esmad.ipp.pt>
2021-06-24 00:53:15 +0100

    deploy

---
+++ precache-manifest.d12641c4274941136d62d496dba03a5f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cc940549204c0c12f53",
+    "url": "/css/app.9e8318a0.css"
+  },
+  {
+    "revision": "d93e664cf9c06b99e064b584b4b52333",
+    "url": "/img/bkg.d93e664c.png"
+  },
+  {
+    "revision": "03ab09309c047e622a77721e5d5d8298",
+    "url": "/img/fcm.03ab0930.png"
+  },
+  {
+    "revision": "609848dfe2d6fcf0dc6725c315ac71bf",
+    "url": "/img/impetus.609848df.png"
+  },
+  {
+    "revision": "3581b94f67443293dd44059a7d94b671",
+    "url": "/img/logoAnime.3581b94f.svg"
+  },
+  {
+    "revision": "8bc0d4ee27a4743dcbf55e1b6a5d7951",
+    "url": "/img/me2.8bc0d4ee.png"
+  },
+  {
+    "revision": "8dd6c83906783456aecc4e78bd0f2652",
+    "url": "/img/me3.8dd6c839.png"
+  },
+  {
+    "revision": "4e550538e62e5aa3fa0002230309030f",
+    "url": "/img/ondisplay.4e550538.png"
+  },
+  {
+    "revision": "73ab2c124f8f11b78089eb4ad8466214",
+    "url": "/img/pointer.73ab2c12.png"
+  },
+  {
+    "revision": "0f7ae3dee36654e5b197b965389a689e",
+    "url": "/img/tetris.0f7ae3de.png"
+  },
+  {
+    "revision": "b0182829c5edbf3ec4551fb6805863c3",
+    "url": "/img/winehead.b0182829.png"
+  },
+  {
+    "revision": "c80a128d6e2097bbb8a4de997d876888",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cc940549204c0c12f53",
+    "url": "/js/app.b174517c.js"
+  },
+  {
+    "revision": "181375999207959485e5",
+    "url": "/js/chunk-vendors.296f90e3.js"
+  },
+  {
+    "revision": "555a5e237588cac3080298654dd97579",
+    "url": "/manifest.json"
+  }
+]);
